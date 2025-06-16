e485f12e68a8b6feed51ab77a29ad72ee7191933
Gura <contactogura1@gmail.com>
2025-06-16 11:48:08 -0500

    Removed unused imports

--- src/main/java/net/gura/papaCaliente/game/GamePlayer.java
+++ src/main/java/net/gura/papaCaliente/game/GamePlayer.java
@@ -2,16 +2,13 @@ package net.gura.papaCaliente.game;
 
 import net.gura.papaCaliente.PapaCaliente;
 import net.gura.papaCaliente.utils.CustomItems;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
